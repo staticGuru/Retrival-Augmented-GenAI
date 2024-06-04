8e6353312e90f4efccd9ceedde33f53a6868cd94
staticGuru <vigneshguru274@gmail.com>
2024-06-04 16:19:55 +0530

    add the dot env sections

--- insert_data.py
+++ insert_data.py
@@ -3,3 +3,5 @@ from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import PGVector
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
+
+load_dotenv(find_dotenv())
