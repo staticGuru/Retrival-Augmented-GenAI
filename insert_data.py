e28cf5db1f7eb0d5a2db558602a7345f327bd9b4
staticGuru <vigneshguru274@gmail.com>
2024-06-04 16:19:23 +0530

    add the package import sections

---
+++ insert_data.py
@@ -0,0 +1,5 @@
+from dotenv import find_dotenv, load_dotenv
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import PGVector
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
