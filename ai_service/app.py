91aa73722e2094d707189f976ee6697301fab66a
staticGuru <vigneshguru274@gmail.com>
2024-06-04 15:27:21 +0530

    import packages

--- ai_service/app.py
+++ ai_service/app.py
@@ -4,8 +4,17 @@ from pydantic import BaseModel
 import os
 from fastapi.middleware.cors import CORSMiddleware
 
-
 import logging
+from dotenv import find_dotenv, load_dotenv
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores.pgvector import PGVector
+from langchain.chat_models import ChatOpenAI
+
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+)
 
 
 
